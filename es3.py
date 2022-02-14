bb8f9144b76f7262879f7ca387702ae5b1ea383b
Devid Rosa <78610638+Devid-developer@users.noreply.github.com>
2022-02-14 15:18:09 +0000

    funzia

--- es3.py
+++ es3.py
@@ -1,16 +1,22 @@
 class CSVFile:
-    def __str__(self, file, nome):
+    def __init__(self, file, nome):
         self.name = nome
         self.file = file
 
     def get_data(self):
         data = []
         for line in self.file:
-            if line != 'Date,Sales':
-                data.append(line)
+            elements = line.split(',')
+            if elements[0] != 'Date':
+                tmp = [elements[0], float(elements[1])]
+                data.append(tmp)
+                
         return data
 
-open('shampoo_sales.csv')
+my_file = open('shampoo_sales.csv')
+test = CSVFile(my_file, 'shampoo_sales')
+print(test.get_data())
+my_file.close()
 
 
             
